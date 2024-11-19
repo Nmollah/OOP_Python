83b3ec8225091ed111c2c1f38bdcc59e411b9121
Md Nur Nobi <145796865+Nmollah@users.noreply.github.com>
2024-11-19 15:20:51 +0600

    add main program

--- BookShop.py
+++ BookShop.py
@@ -17,3 +17,18 @@ class BookRentShop:
             self.book=self.book-q
             print("Total Price", q*10)
             print("Total No of stocks",self.book)
+
+while True:
+    shop1=BookRentShop(100)
+    ui=int(input('''inter a input
+    1 =Display Books Stocks
+    2 =Rent Book
+    3 = exit
+    '''))
+    if ui==1:
+        print("Book in Store",shop1.displayBook())
+    elif ui==2:
+        n=int(input('Enter the number of book'))
+        shop1.rentBook(n)
+    else:
+        break
